3cfca3ab660171848e248ce56153203e5ba531be
Prashanth A <prashantag144@gmail.com>
2026-07-01 05:48:45 +0000

    numpy basics is completed

---
+++ OOPS.py
@@ -0,0 +1,20 @@
+""" CLASS  """
+class Car:
+    brand="TOYOTO" #attributes
+    def hello(self):
+        print("how are you") #methods
+
+    print("hello how are you , i am getting initialise")
+
+
+""" print(Car.brand)
+(Car.hello()) """
+
+
+
+obj=Car()
+obj1=Car()
+obj2=Car()
+
+print(obj.brand)
+obj.hello()
